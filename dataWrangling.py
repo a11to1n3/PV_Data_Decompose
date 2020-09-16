8b551a985167a8ad981cc2f76894f4bdbc25b944
duyanhpham@outlook.com <duyanhpham@outlook.com>
2020-09-16 23:25:16 +0200

    added hour var

--- dataWrangling.py
+++ dataWrangling.py
@@ -12,6 +12,30 @@ def dataWrangling(pv_raw):
   pv_raw['Sun'] = np.zeros(len(pv_raw))
 
   # Date arranging initiation
+  pv_raw['h01'] = np.zeros(len(pv_raw))
+  pv_raw['h02'] = np.zeros(len(pv_raw))
+  pv_raw['h03'] = np.zeros(len(pv_raw))
+  pv_raw['h04'] = np.zeros(len(pv_raw))
+  pv_raw['h05'] = np.zeros(len(pv_raw))
+  pv_raw['h06'] = np.zeros(len(pv_raw))
+  pv_raw['h07'] = np.zeros(len(pv_raw))
+  pv_raw['h08'] = np.zeros(len(pv_raw))
+  pv_raw['h09'] = np.zeros(len(pv_raw))
+  pv_raw['h10'] = np.zeros(len(pv_raw))
+  pv_raw['h11'] = np.zeros(len(pv_raw))
+  pv_raw['h12'] = np.zeros(len(pv_raw))
+  pv_raw['h13'] = np.zeros(len(pv_raw))
+  pv_raw['h14'] = np.zeros(len(pv_raw))
+  pv_raw['h15'] = np.zeros(len(pv_raw))
+  pv_raw['h16'] = np.zeros(len(pv_raw))
+  pv_raw['h17'] = np.zeros(len(pv_raw))
+  pv_raw['h18'] = np.zeros(len(pv_raw))
+  pv_raw['h19'] = np.zeros(len(pv_raw))
+  pv_raw['h20'] = np.zeros(len(pv_raw))
+  pv_raw['h21'] = np.zeros(len(pv_raw))
+  pv_raw['h22'] = np.zeros(len(pv_raw))
+  pv_raw['h23'] = np.zeros(len(pv_raw))
+
   pv_raw['01'] = np.zeros(len(pv_raw))
   pv_raw['02'] = np.zeros(len(pv_raw))
   pv_raw['03'] = np.zeros(len(pv_raw))
@@ -59,6 +83,53 @@ def dataWrangling(pv_raw):
 
   # Data wrangling
   for i in range(len(pv_raw)):
+    if pv_raw.Datetime[i][10:12] == '01':
+      pv_raw['h01'][i] = 1
+    elif pv_raw.Datetime[i][10:12] == '02':
+      pv_raw['h02'][i] = 1
+    elif pv_raw.Datetime[i][10:12] == '03':
+      pv_raw['h03'][i] = 1
+    elif pv_raw.Datetime[i][10:12] == '04':
+      pv_raw['h04'][i] = 1
+    elif pv_raw.Datetime[i][10:12] == '05':
+      pv_raw['h05'][i] = 1
+    elif pv_raw.Datetime[i][10:12] == '06':
+      pv_raw['h06'][i] = 1
+    elif pv_raw.Datetime[i][10:12] == '07':
+      pv_raw['h07'][i] = 1
+    elif pv_raw.Datetime[i][10:12] == '08':
+      pv_raw['h08'][i] = 1
+    elif pv_raw.Datetime[i][10:12] == '09':
+      pv_raw['h09'][i] = 1
+    elif pv_raw.Datetime[i][10:12] == '10':
+      pv_raw['h10'][i] = 1
+    elif pv_raw.Datetime[i][10:12] == '11':
+      pv_raw['h11'][i] = 1
+    elif pv_raw.Datetime[i][10:12] == '12':
+      pv_raw['h12'][i] = 1
+    elif pv_raw.Datetime[i][10:12] == '13':
+      pv_raw['h13'][i] = 1
+    elif pv_raw.Datetime[i][10:12] == '14':
+      pv_raw['h14'][i] = 1
+    elif pv_raw.Datetime[i][10:12] == '15':
+      pv_raw['h15'][i] = 1
+    elif pv_raw.Datetime[i][10:12] == '16':
+      pv_raw['h16'][i] = 1
+    elif pv_raw.Datetime[i][10:12] == '17':
+      pv_raw['h17'][i] = 1
+    elif pv_raw.Datetime[i][10:12] == '18':
+      pv_raw['h18'][i] = 1
+    elif pv_raw.Datetime[i][10:12] == '19':
+      pv_raw['h19'][i] = 1
+    elif pv_raw.Datetime[i][10:12] == '20':
+      pv_raw['h20'][i] = 1
+    elif pv_raw.Datetime[i][10:12] == '21':
+      pv_raw['h21'][i] = 1
+    elif pv_raw.Datetime[i][10:12] == '22':
+      pv_raw['h22'][i] = 1
+    elif pv_raw.Datetime[i][10:12] == '23':
+      pv_raw['h23'][i] = 1
+
     pv_raw[pv_raw.Datetime[i][:2]][i] = 1
 
     if pv_raw.Datetime[i][4:5] == '1':
